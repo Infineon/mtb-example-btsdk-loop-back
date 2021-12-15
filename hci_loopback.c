f841b60a0bb9c40557fa450c7793a51214970ae0
gitlab-runner <do-not-reply@cypress.com>
2021-12-14 18:31:30 -0600

    Upload mtb-example-btsdk-loop-back 3.2.0.20467

--- hci_loopback.c
+++ hci_loopback.c
@@ -225,16 +225,10 @@ hci_test_app_management_cback(wiced_bt_management_evt_t event, wiced_bt_manageme
 void hci_test_init()
 {
 #ifdef WICED_BT_TRACE_ENABLE
-    wiced_result_t status;
-
     WICED_BT_TRACE("Init test\n");
     /* Starting the app timer */
     wiced_init_timer(&test_timer, hci_test_timeout, 0, WICED_SECONDS_PERIODIC_TIMER);
-    status = wiced_start_timer(&test_timer, 1);
-    if (status != WICED_SUCCESS)
-    {
-        WICED_BT_TRACE("%s: wiced_start_timer failed, status:%d \n", __func__, status);
-    }
+    wiced_start_timer(&test_timer, 1);
 
     /* Register callback for receiving hci traces */
     wiced_bt_dev_register_hci_trace(hci_test_trace_callback);
